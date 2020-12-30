61b19df38def4e05257e4a893a3cb43aa3ae37a9
jeremysamuel13 <70209502+jeremysamuel13@users.noreply.github.com>
2020-12-29 20:55:44 -0500

    bot things

--- bot.py
+++ bot.py
@@ -7,8 +7,6 @@ apiSecretKey = "enyyDKXNPt3nwwyEEPirq94lGpDSkENdRtmLJiCdHKjF2YHNY5"
 apiAccessToken = "1343749282499854338-egzvNHmwrm9i0Y3o73z3L3usvIq7Kb"
 apiSecretAccessToken = "ffJon9xD1C8tKaJfhYozdSRfyqVg1X8X8tp6lF9MPWz3b"
 
-screen_name = "compReverse"
-
 # twtAcc @OffTheSenzu
 myAccId = "3408914651"
 # other twt accounts
@@ -21,7 +19,7 @@ class MyStreamListener(tweepy.StreamListener):
         tweet = "@" + status.user.screen_name + " " + self.reversify(status.text)
         b = True
 
-        if screen_name in tweet:
+        if api.auth.get_username() in tweet:
             b = False
 
         if b:
