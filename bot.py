e3ba236dd858d9281fc3b906241e29c46662914c
jeremysamuel13 <70209502+jeremysamuel13@users.noreply.github.com>
2021-01-01 20:07:34 -0500

    added a config file

--- bot.py
+++ bot.py
@@ -2,18 +2,23 @@ import tweepy
 import upsidedown
 import json
 
+# INPUT INFORMATION IN 'config.json' IN ORDER TO HAVE THE BOT PROPERLY WORKING
+
+# loading config file
 with open("config.json", "r") as f:
     config = json.load(f)
 
+# twitter auth stuff
 apiKey = config["apiKey"]
 apiSecretKey = config["apiSecretKey"]
-
 apiAccessToken = config["apiAccessToken"]
 apiSecretAccessToken = config["apiSecretAccessToken"]
 
+# accounts to follow (refer to config comments)
 accIDs = config["accIDs"]
 
 
+# Tweepy stuff
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
@@ -37,10 +42,20 @@ class MyStreamListener(tweepy.StreamListener):
     def reversify(inp):
         return upsidedown.transform(inp)
 
-auth = tweepy.OAuthHandler(apiKey, apiSecretKey)
-auth.set_access_token(apiAccessToken, apiSecretAccessToken)
-api = tweepy.API(auth, wait_on_rate_limit=True)
 
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-myStream.filter(follow=accIDs)
+# main
+if not (apiKey == "key1" or
+        apiSecretKey == "key2" or
+        apiAccessToken == "token1" or
+        apiSecretAccessToken == "token2"):
+
+    auth = tweepy.OAuthHandler(apiKey, apiSecretKey)
+    auth.set_access_token(apiAccessToken, apiSecretAccessToken)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    myStreamListener = MyStreamListener()
+    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+    myStream.filter(follow=accIDs)
+
+else:
+    print("Invalid config file")
